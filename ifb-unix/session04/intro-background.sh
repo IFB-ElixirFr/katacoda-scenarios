ed4420b858eef44c2f517279dd4b152980019880
clairetn <clairetn@users.noreply.github.com>
2022-04-27 10:30:08 +0200

    Update intro-background.sh

--- ifb-unix/session04/intro-background.sh
+++ ifb-unix/session04/intro-background.sh
@@ -8,6 +8,7 @@
 # bed toy hg38
 wget -O hg38_exons.bed.gz https://zenodo.org/record/5716151/files/hg38_exons.bed.gz?download=1
 gunzip hg38_exons.bed.gz
-mv hg38_exons.bed data/.
 
 bash .scripts/intro-background_shared_bank.sh
+
+mv hg38_exons.bed /shared/bank/homo_sapiens/hg38/.
