3d300710b9812f700d125b1acdf01c2200459a8d
clairetn <clairetn@users.noreply.github.com>
2022-04-27 10:39:05 +0200

    Update intro-background.sh

--- ifb-unix/session04/intro-background.sh
+++ ifb-unix/session04/intro-background.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+bash .scripts/intro-background_shared_bank.sh
+
 # RNAseq toy A.thaliana + fna + gff
 # wget -O data.tar.gz https://zenodo.org/record/3997237/files/FAIR_Bioinfo_data.tar.gz?download=1
 # tar -xzf data.tar.gz
@@ -8,7 +10,4 @@
 # bed toy hg38
 wget -O hg38_exons.bed.gz https://zenodo.org/record/5716151/files/hg38_exons.bed.gz?download=1
 gunzip hg38_exons.bed.gz
-
-bash .scripts/intro-background_shared_bank.sh
-
 mv hg38_exons.bed /shared/bank/homo_sapiens/hg38/bed/.
